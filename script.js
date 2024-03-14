605bf150e4f1205074621e63df9953c322b89058
Lecheeky <thadjlakehal@gmail.com>
2024-03-14 14:27:31 +0000

    if else statement that applies classes to inpu area depending on the time

--- script.js
+++ script.js
@@ -15,5 +15,22 @@ const scheduleElements = () => {
         const timeBlockTime = $("<p></p>").addClass("container hour time-block")
         const timeBlockUserText = $("<input></input>").addClass("row textarea description")
         const timeBlockBtn = $("<button>Save</button>").addClass("saveBtn i:hover")
+        
+        if(i <= 11 ) {
+            timeBlockTime.text(`${i}am`)
+        } else {
+            timeBlockTime.text(`${i}pm`)
+        }
+
+        if (dayjs().format("H") < i){
+            timeBlockUserText.addClass("future")
+        } else if (Math.floor(dayjs().format("H")) === i ){
+            timeBlockUserText.addClass("present")
+        } else {
+            timeBlockUserText.addClass("past")
+        }
+
+        timeBlockDiv.append(timeBlockTime, timeBlockUserText, timeBlockBtn)
+        timeBlock.append(timeBlockDiv)
     }
 }
