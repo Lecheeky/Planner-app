5d683d326b19768c2adf97304ea72a5b7a89eff9
Lecheeky <thadjlakehal@gmail.com>
2024-03-14 14:26:30 +0000

    adding function that takes start time to endtime, and a for loop that contains queryselectors targetting html elements

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const timeBlock = $(".container")
 
 const currentDay = dayjs().format("dddd MMMM D")
 currentDayText.append(currentDay)
+
+const scheduleElements = () => {
+
+    const startTime = 9 
+    const endTime = 17
+
+    for (let i = startTime; i <= endTime ; i++) {
+        
+        const timeBlockDiv = $("<div></div>").addClass("container time-block-container")
+        const timeBlockTime = $("<p></p>").addClass("container hour time-block")
+        const timeBlockUserText = $("<input></input>").addClass("row textarea description")
+        const timeBlockBtn = $("<button>Save</button>").addClass("saveBtn i:hover")
+    }
+}
